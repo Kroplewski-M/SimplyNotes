ec3a09532eb36cda11e85d6df841037613b9fb9a
Mateusz <kroplewskimateusz@gmail.com>
2023-02-03 07:53:13 +0000

    auth page responsive

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -2,26 +2,23 @@ import { useState } from "react";
 
 
 const Auth = () =>{
-    const [register,setRegister] = useState(false);
-    function printReg(){
-        setRegister(false);
-        console.log('register');
-    }
+    const [register,setRegister] = useState(true);
+
     return(
-            <div className="w-[300px] md:w-[500px] h-[550px] rounded-md bg-lightBrown mx-auto mt-5">
+            <div className="w-[300px] md:w-[500px] rounded-md bg-lightBrown mx-auto mt-5">
                 {register ?
                 (
                 <div>
                     <p className="font-bold text-center text-[20px] md:text-[25px] pt-[5px]">Register</p>
-                    <form action="" className="mt-5 w-[300px]  grid place-content-center items-center">
+                    <form action="" className="mt-5 w-[300px] md:w-[500px] grid place-content-center items-center">
                         <label htmlFor="name" className="font-semibold mr-[5px]">Full Name:</label>
-                        <input type="text" name="name" id="" className="w-[200px] rounded-md bg-gray-200 pl-[5px] mb-[10px]"/>
+                        <input type="text" name="name" id="" className="w-[200px] md:w-[250px] md:h-[30px] rounded-md bg-gray-200 pl-[5px] mb-[10px]"/>
 
                         <label htmlFor="email" className="font-semibold mr-[5px]">Email:</label>
-                        <input type="email" name="email" id="" className="w-[200px] rounded-md bg-gray-200 pl-[5px] mb-[10px]"/>
+                        <input type="email" name="email" id="" className="w-[200px] md:w-[250px] md:h-[30px] rounded-md bg-gray-200 pl-[5px] mb-[10px]"/>
 
                         <label htmlFor="password" className="font-semibold mr-[5px]">Password:</label>
-                        <input type="password" name="password" id="" className="w-[200px] rounded-md bg-gray-200 pl-[5px] mb-[10px]"/>
+                        <input type="password" name="password" id="" className="w-[200px] md:w-[250px] md:h-[30px] rounded-md bg-gray-200 pl-[5px] mb-[10px]"/>
                         
                         <label htmlFor="use" className="w-[200px] font-semibold text-center">What are you going to use this app for?</label>
                         <select name="use" id="" className="mt-5 bg-gray-200 rounded-md font-semibold mb-10">
@@ -33,7 +30,7 @@ const Auth = () =>{
                         <p className="text-center font-bold mt-5">Or</p>
                     </form>
                     <div className="w-[200px] mx-auto">
-                        <button className="w-[100%] h-[35px] md:h-[50px] rounded-md bg-[#333333] text-gray-200 hover:bg-gray-300 hover:text-[#333333] font-semibold mt-5"
+                        <button className="w-[100%] h-[35px] md:h-[50px] rounded-md bg-[#333333] text-gray-200 hover:bg-gray-300 hover:text-[#333333] font-semibold mt-5 mb-10"
                         onClick={()=> setRegister(!register)}>Log In</button>
                     </div>
                 </div>
@@ -41,19 +38,19 @@ const Auth = () =>{
                 (
                     <div>
                         <p className="font-bold text-center text-[20px] md:text-[25px] pt-[5px]">Login</p>
-                        <form action="" className="mt-5 w-[300px]  grid place-content-center items-center">
+                        <form action="" className="mt-5 w-[300px] md:w-[500px]  grid place-content-center items-center">
                             <label htmlFor="email" className="font-semibold mr-[5px]">Email:</label>
-                            <input type="email" name="email" id="" className="w-[200px] rounded-md bg-gray-200 pl-[5px] mb-[10px]"/>
+                            <input type="email" name="email" id="" className="w-[200px] rounded-md bg-gray-200 pl-[5px] mb-[10px] md:w-[250px] md:h-[30px]"/>
 
                             <label htmlFor="password" className="font-semibold mr-[5px]">Password:</label>
-                            <input type="password" name="password" id="" className="w-[200px] rounded-md bg-gray-200 pl-[5px] mb-[10px]"/>
+                            <input type="password" name="password" id="" className="w-[200px] rounded-md bg-gray-200 pl-[5px] mb-[10px] md:w-[250px] md:h-[30px]"/>
 
                             <button className=" mt-10 w-[100%] h-[35px] md:h-[50px] rounded-md bg-[#333333] text-gray-200 hover:bg-gray-300 hover:text-[#333333] font-semibold">
                             Login</button>
                             <p className="text-center font-bold mt-5">Or</p>
                         </form>
                         <div className="w-[200px] mx-auto">
-                            <button className="w-[100%] h-[35px] md:h-[50px] rounded-md bg-[#333333] text-gray-200 hover:bg-gray-300 hover:text-[#333333] font-semibold mt-5"
+                            <button className="w-[100%] h-[35px] md:h-[50px] rounded-md bg-[#333333] text-gray-200 hover:bg-gray-300 hover:text-[#333333] font-semibold mt-5 mb-10"
                             onClick={()=> setRegister(!register)}>Register</button>
                         </div>
                     </div>
