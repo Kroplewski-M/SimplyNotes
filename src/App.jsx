15ce0687d309c6361a3bd82af75229698e7ae99e
Mateusz <kroplewskimateusz@gmail.com>
2023-02-10 01:46:08 +0000

    set global user

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,27 @@ import './App.css';
 import Nav from './components/Nav';
 import Home from './pages/Home';
 import Auth from './pages/Auth';
+import { UserContext } from './userContext';
+import { useState } from 'react';
+
 function App() {
-  
+  const [user, setUser] = useState(null);
 
   return (
     <div className="App">
+      <UserContext.Provider value={{user,setUser}}>
       <Nav />
       {/* <Home /> */}
       <Auth />
+      </UserContext.Provider>
     </div>
   )
 }
 
 export default App
+
+
+//TO DO
+//FETCH LOGGED IN USER WHEN RELOAD
+//FETCH USER INFO FROM TABLE AND SET TO GLOBAL VAR
+//SET GLOBAL VAR WITH USER INFO WHEN REGISTERED
