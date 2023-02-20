6713a31d607a0a5e5967e5baa2978e73fea61553
Mateusz <kroplewskimateusz@gmail.com>
2023-02-20 10:58:11 +0000

    started working on projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,7 +4,17 @@ const Projects = () =>{
     
 
     return(
-        <p>Projects</p>
+        <>
+            <div className="w-[300px] mx-auto mt-10">
+                <p className="font-bold text-center text-[20px]">Notes:</p>
+
+                <div className="w-[200px] h-[150px] bg-gray-300 mx-auto mt-10 rounded-md grid place-content-center">
+                    <div className="w-[50px]">
+                        <img src="../src/assets/plus.png" alt="" className="w-[100%]"></img>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
